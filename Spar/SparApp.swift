f34e30198fe100c9d459ca62676573428358d33e
Ilya Pavlov <ilyapav@icloud.com>
2024-01-30 21:45:27 +0300

    Initial Commit

---
+++ Spar/SparApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparApp.swift
+//  Spar
+//
+//  Created by ily.pavlov on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
